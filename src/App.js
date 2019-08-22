873862aef24bab9e2dbfe563032b98401043f946
Hristina Dakic <hristina.dakic@gmail.com>
2019-08-22 09:35:58 +0200

    start the todo list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import TodoList from './TodoList'
+import TodoItems from './TodoItems'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  inputElement = React.createRef() //To refer to the input, we create a ref with inputElement =React.createRef().
+  constructor() {//React.Component is the base class for all class based React JS components. Whenever a class inherits the class React.Component its constructor will automatically assign attribute state to the class with initial value set to null. We can change it by overriding the method constructor.
+ 
+   super()
+   this.state = {
+     items: [],
+     currentItem: {
+       text: '',
+       key: '',
+     },
+   }
+ }
+
+ // deleteItem filters the received key from the items state. Then sets the items to filtered items.
+ deleteItem = key => {
+   const filteredItems = this.state.items.filter(item => {
+     return item.key !== key
+   })
+   this.setState({
+     items: filteredItems,
+   })
+ }
+
+ handleInput = e => { //The handleInput gets the event
+   const itemText = e.target.value //it gets the value from the input box 
+   const currentItem = { text: itemText, key: Date.now() } // It makes a new const "currentItem", that has key as current date and text as the input data. The key is Date.now() which is the number of milliseconds from 1970 to now. It can only take a maximum of 1 input per millisecond. 
+   this.setState({ //it sets the state to the object of currentItem.
+     currentItem,
+   })
+ }
+
+ addItem = e => {
+   e.preventDefault() //The preventDefault will prevent reloading from submitting the form.
+   const newItem = this.state.currentItem //It gets the value in the input box from the state currentItem.
+   if (newItem.text !== '') { //Since we do not want to add empty value to our todo, we check that.
+     const items = [...this.state.items, newItem] //If it’s not empty, items array is destructured and newItem is added. 
+     this.setState({ //We have to set items[] to the state, we call this.setSate.
+       items: items,
+       currentItem: { text: '', key: '' }, // It also makes sense to reset the currentItem to clear the input box.
+     })
+   }
+ }
+
+ render() {
+   return (
+     <div className="App">
+       <TodoList
+         addItem={this.addItem}
+         inputElement={this.inputElement} // used for setting the focus on the input field
+         handleInput={this.handleInput} // is called everytime the input box content changes
+         currentItem={this.state.currentItem}
+       />
+       <TodoItems
+         entries={this.state.items}
+         deleteItem={this.deleteItem} //we have created a new property in App.js as deleteItem.
+       />
+     </div>
+   )
+ }
 }
 
-export default App;
+export default App
+
---
+++ src/TodoItems.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class TodoItems extends Component { //Component TodoItems pass all items as a prop.
+ createTasks = item => { //The function createTasks returns li for each passed item. It uses the key we provided earlier. 
+   return ( 	//Adding an onClick listener to deleteItem with the key.  This executes deleteItem with the key as a parameter.
+     <li key={item.key} onClick={() => this.props.deleteItem(item.key)}>
+       {item.text}
+     </li>
+   )
+ }
+ render() {
+   const todoEntries = this.props.entries
+   const listItems = todoEntries.map(this.createTasks) //All these list items are saved to listItems using a mapping function. This gets used in the ul in return statement.
+
+
+
+   return <ul className="theList">{listItems}</ul>
+ }
+}
+
+export default TodoItems
