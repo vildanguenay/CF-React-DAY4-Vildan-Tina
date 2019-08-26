6950df1ab9b63cb40bd4edc39791dfa01e368355
Hristina Dakic <hristina.dakic@gmail.com>
2019-08-26 15:07:12 +0200

    made changes to the input fields

--- src/App.js
+++ src/App.js
@@ -1,70 +1,48 @@
 import React, { Component } from 'react';
-import TodoList from './TodoList';
-import TodoItems from './TodoItems';
-import './App.css';
+import './form.css';
 
-class App extends Component {
-  inputElement = React.createRef() //To refer to the input, we create a ref with inputElement =React.createRef().
+class Form extends Component {
+  
   constructor() {//React.Component is the base class for all class based React JS components. Whenever a class inherits the class React.Component its constructor will automatically assign attribute state to the class with initial value set to null. We can change it by overriding the method constructor.
- 
-   super()
+   super();
    this.state = {
-     items: [],
-     currentItem: {
-       text: '',
-       key: '',
-     },
-   }
+       first_name: '',
+       last_name: '',
+       age: ''
+     };
+
+     this.handleChange = this.handleChange.bind(this); //to connect data with the event handler (data binding)
+     this.handleSubmit = this.handleSubmit.bind(this);
  }
 
- // deleteItem filters the received key from the items state. Then sets the items to filtered items.
- deleteItem = key => {
-   const filteredItems = this.state.items.filter(item => {
-     return item.key !== key
-   })
-   this.setState({
-     items: filteredItems,
-   })
- }
+  handleChange(evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+      console.log(evt.target.value);
+  }
 
- handleInput = e => { //The handleInput gets the event
-   const itemText = e.target.value //it gets the value from the input box 
-   const currentItem = { text: itemText, key: Date.now() } // It makes a new const "currentItem", that has key as current date and text as the input data. The key is Date.now() which is the number of milliseconds from 1970 to now. It can only take a maximum of 1 input per millisecond. 
-   this.setState({ //it sets the state to the object of currentItem.
-     currentItem,
-   })
- }
+  handleSubmit(evt) {
+    alert('Hello ' + this.state.first_name + " " + this.state.last_name + "! How are you? " + "Your age is: " + this.state.age);
+    evt.preventDfault();
+  }
 
- addItem = e => {
-   e.preventDefault() //The preventDefault will prevent reloading from submitting the form.
-   const newItem = this.state.currentItem //It gets the value in the input box from the state currentItem.
-   if (newItem.text !== '') { //Since we do not want to add empty value to our todo, we check that.
-     const items = [...this.state.items, newItem] //If it’s not empty, items array is destructured and newItem is added. 
-     this.setState({ //We have to set items[] to the state, we call this.setSate.
-       items: items,
-       currentItem: { text: '', key: '' }, // It also makes sense to reset the currentItem to clear the input box.
-     })
-   }
- }
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit} className="form">
+        <label>First Name</label>
+        <input type="text" name="first_name" onChange = {this.handleChange} />
 
- render() {
-   return (
-    <div className ="App">
-    <h1>Business ToDo List</h1>
-       <TodoList
-         addItem={this.addItem}
-         inputElement={this.inputElement} // used for setting the focus on the input field
-         handleInput={this.handleInput} // is called everytime the input box content changes
-         currentItem={this.state.currentItem}
-       />
-       <TodoItems
-         entries={this.state.items}
-         deleteItem={this.deleteItem} //we have created a new property in App.js as deleteItem.
-       />
-    </div>
-   )
- }
+        <label>Last Name</label>
+        <input type="text" name="last_name" onChange = {this.handleChange}/>
+
+        <label>Age</label>
+        <input type="text" name="age" onChange = {this.handleChange}/>
+
+        <input type="submit" value="Submit"/>
+
+      </form>
+    );
+  }
 }
 
-export default App
+export default Form;
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppSup from './AppSup';
+import Form from './App';
+//import App2 from './App2';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AppSup />, document.getElementById('root'));
+ReactDOM.render(<Form />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
